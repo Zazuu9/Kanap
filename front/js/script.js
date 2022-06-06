a804b5b88eea09a203c159e9aa2d18915bb3ac7e
Kevin Gouaille <kevingouaille8515@gmail.com>
2022-06-06 17:18:04 +0200

    Ajout alert script.js

--- front/js/script.js
+++ front/js/script.js
@@ -9,12 +9,16 @@ fetch("http://localhost:3000/api/products")
 .then((data) => {
      for (let object in data) {
          document.getElementById('items').innerHTML +=
-         `<a href="${object._id}">
+         `<a href="${data[object]._id}">
             <article>
-              <img src="${object.imageUrl}" alt="${object.altTxt}">
-              <h3 class="productName">${object.name}</h3>
-              <p class="productDescription">${object.description}</p>
+              <img src="${data[object].imageUrl}" alt="${data[object].altTxt}">
+              <h3 class="productName">${data[object].name}</h3>
+              <p class="productDescription">${data[object].description}</p>
             </article>
           </a>`
      }                   
+})
+
+.catch((error)=> {
+    alert("Une errreur est survenue")
 })
