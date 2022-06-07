ff61753a8827a53dd2f6b85671d3530ee255e5eb
Kevin Gouaille <kevingouaille8515@gmail.com>
2022-06-07 17:19:29 +0200

    Ajout image et nom canapé

---
+++ front/js/product.js
@@ -0,0 +1,25 @@
+var url = window.location.search;
+console.log(url);
+var id = url.substring(4);
+console.log(id);
+
+fetch(`http://localhost:3000/api/products/${id}`)
+
+.then(function(res) {
+    if(res.ok) {
+        return res.json();
+    }
+})
+
+.then((object) => {
+    document.querySelector('.item__img').innerHTML =
+    `<img src="${object.imageUrl}" alt="Photographie d'un canapé">`;
+
+    document.getElementById('title').innerText =
+    object.name;
+    
+})
+
+.catch((error) => {
+    console.log(error);
+})
